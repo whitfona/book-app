f48ad2dc10ef946946f8e6299f28f2e3b6b76d7a
Nick <nicholas@wealthyplanet.com>
2021-06-16 10:33:58 -0400

    Added JWT

---
+++ back-end/JTW.js
@@ -0,0 +1,28 @@
+const {sign, verify} = require('jsonwebtoken');
+require('dotenv').config();
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+const createToken = (user) => {
+  const accessToken = sign({ username: user.username }, JWT_SECRET);
+
+  return accessToken;
+}
+
+const validateToken = (req, res, next) => {
+  const accessToken = req.cookies["reading-token"];
+
+  if (!accessToken) return res.status(400).json({ error: 'User not authenticated.'});
+
+  try {
+    const validToken = verify(accessToken, JWT_SECRET)
+    if (validToken) {
+      req.authenticated = true
+      return next();
+    } 
+  } catch(err) {
+      return res.status(400).json({ error: err });
+  }
+}
+
+module.exports = { createToken, validateToken }
